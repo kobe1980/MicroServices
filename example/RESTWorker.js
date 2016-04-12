ddb5052f22cc5e8fb039a7be7c3dad067eec334f
kobe1980 <kobe1980@gmail.com>
2016-04-12 19:42:37 +0200

    Simple REST Worker v0

--- example/RESTWorker.js
+++ example/RESTWorker.js
@@ -1,14 +1,48 @@
 // Child class of Worker dedicated to REST connection
 var Worker = require("../Worker.js");
 var logger = require("../logger.js");
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+var requests = Array();
 
 var RESTW = new Worker("WA");
 RESTW.treatError = function(error) {
 	logger. log("MicroService", "Worker A - REST connector", "Error received");
+	oError = JSON.parse(error);
+	var response = requests[oError.data.resId].res;
+	response.writeHead(500);
+	response.end();
 }
 
 RESTW.doJob = function(data) {
 	logger.log("MicroService", "Worker A - REST connector", "Job to do: "+JSON.stringify(data));
+	oData = JSON.parse(data);
+	var response = requests[oData.resId].res;
+	response.writeHead(200);
+	response.end(oData.content);
+	requests.splice(oData.resId, 1);
 }
 
-setTimeout(function() {RESTW.sendToNextWorker(["WA", "WB"], {title: "toto"});}, 500);
+var server = http.createServer(function(req, res) {
+	switch(req.method) {
+	case 'GET':
+	case 'POST':
+		var path = url.parse(req.url).pathname;
+		if (path == '/movies/') {
+			var resId = requests.push({req: req, res: res})-1;
+			RESTW.sendToNextWorker(["WD"], {method: req.method, params: querystring.parse(url.parse(req.url).query), resId: resId});
+		} else send404(res);
+		break;
+	default: 
+		send404(res);
+	}
+
+});
+
+function send404(res) {
+	res.writeHead(404);
+	res.end();
+}
+
+server.listen(8080);
